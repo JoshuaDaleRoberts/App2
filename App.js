06ab6ef1cab81decb707267ae28d3ddb34a01bc1
JoshuaDaleRoberts <96258127+JoshuaDaleRoberts@users.noreply.github.com>
2024-01-13 15:38:14 -0800

    EVERYTHING WORKS

--- App.js
+++ App.js
@@ -15,16 +15,15 @@ implement monosyllabic verbs
 const myApp = () => {
   const [isEnabled, setIsEnabled] = useState(false);
   //sets three things, isEnabled is a variable that can be referenced and setIsEnabled is a function that replaces isEnabled with its value. The useState sets the initial value of isEnabled, which is, in this state, false. 
-  const toggleSwitch = () => {
-    setIsEnabled(a => !a)
-  };
+  const toggleSwitch = () => setIsEnabled(a => !a);
   //Declares toggleSwitch as a function which toggles the value of isEnabled. Doing it through setIsEnabled updates the react virtual DOM
+  const [subjectIndex, setSubjectIndex] = useState(0)
+  const [tenseIndex, setTenseIndex] = useState(0);
   const [verbText, setVerbText] = useState("Verb will go here")
+
   //initializes state change for verb output
   const [verb, onChangeText] = React.useState('tembea');
   //initializes state change for verb text input wait what the fuck
-  let subjectIndex = 0;
-  let tenseIndex = 0;
   //initializing values, reset with the selectors, to be used in the conjugateVerb() function. 
   function conjugateVerb(){
     const posSubjectArray = ["ni","u","a","tu","m","wa"]
@@ -45,11 +44,10 @@ const myApp = () => {
       let subjectToUse = negSubjectArray[subjectIndex];
       let tenseToUse = negTenseArray[tenseIndex];
       let tempVerb = verb; 
-      if (endsWithA) {
+      if (endsWithA && tenseIndex === 0) {
         tempVerb = tempVerb.substring(0, verb.length-1) + "i"; 
       }
       running += subjectToUse + tenseToUse + tempVerb
-      console.log(running)
       }
     setVerbText(running)
   }
@@ -60,7 +58,7 @@ const myApp = () => {
         <View style={styles.inline}>
           <Text>Subject:</Text>
           <Dropdown
-            onChange = {(item) => {subjectIndex = item.value}}
+            onChange = {(item) => {setSubjectIndex(item.value)}}
             data = {[
               {label:"Mimi", value:0},
               {label:"Wewe", value:1},
@@ -71,12 +69,13 @@ const myApp = () => {
             ]}
             labelField="label"
             valueField="value"
+            value={subjectIndex}
           />
         </View>
         <View style={styles.inline}>
           <Text>Tense:</Text>
           <Dropdown
-            onChange={(item) => {tenseIndex = item.value;}}
+            onChange={(item) => {setTenseIndex(item.value);}}
             data={[
               {label: "Present", value:0},
               {label:"Past",value:1},
@@ -85,6 +84,7 @@ const myApp = () => {
             ]}
             labelField="label"
             valueField="value"
+            value={tenseIndex}
           />
         </View>
           <View style = {styles.inline}>
